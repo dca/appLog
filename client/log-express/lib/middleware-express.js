9adf6628787f68c40172be6b53f171afd29689c5
Dca <dca35311@gmail.com>
2013-11-29 13:51:04 +0800

    disable parseRequest

--- client/log-express/lib/middleware-express.js
+++ client/log-express/lib/middleware-express.js
@@ -11,3 +11,28 @@ module.exports = function middleware() {
         next();
     };
 };
+
+
+
+
+// function parseRequest(req, kwargs) {
+//     kwargs = kwargs || {};
+
+//     // create absolute url
+//     var host = req.headers.host || '<no host>';
+//     var full_url = (req.socket.encrypted ? 'https' : 'http') + '://' + host + req.url;
+
+//     var http = {
+//         method: req.method,
+//         userAgent : req.headers['user-agent'],
+//         query_string: req.query,
+//         data: req.body || '<unavailable>',
+//         url: full_url,
+//     };
+
+//     var ip = (req.headers['x-forwarded-for'] || '').split(',')[0] ||
+//              req.connection.remoteAddress;
+//     http.REMOTE_ADDR = ip;
+//     kwargs['dlog'] = http;
+//     return kwargs;
+// };
