b3a66a1fee2ddd091d934891dba026ee43667c04
Dca <dca35311@gmail.com>
2013-11-29 17:02:12 +0800

    Update data api

--- webapp/public/javascripts/app/factories.js
+++ webapp/public/javascripts/app/factories.js
@@ -8,22 +8,23 @@ factory('ejs', function(
     ejsResource
 ){
 
-    var ejs = ejsResource('http://localhost:9200');
-    var index = 'dca';
-    var type = 'logger_test';
+    var ejs     = ejsResource('http://localhost:9200');
+    var index   = 'dca';
+    var type    = 'logger_test';
 
-    var ES = ejs.Request().indices(index).types(type);
-
-    function query(argument) {
-        var _result = {};
-        _result = ES
-            .query( ejs.MatchAllQuery() )
-            .size(100)
-            .sort('time', 'desc')
-            .doSearch();
-        return _result;
+    function ESQuery () {
+        return ejs.Request().indices(index).types(type);
     }
 
+
+    function query(argument) {
+        var     ES  = new ESQuery;
+        return  ES
+            .query( ejs.MatchAllQuery() )
+            .size(10)
+            .sort('time', 'desc')
+            .doSearch();
+    }
 
 
     return {
